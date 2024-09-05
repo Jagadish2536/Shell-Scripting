97638a755177200b4bfb745fb9b03420a71a0778
Jagadish2536 <Jagadishvarma99@gmail.com>
2024-09-05 13:04:05 +0530

    added second command

--- variables.sh
+++ variables.sh
@@ -2,5 +2,10 @@
 
 PERSON1=jagadish
 PERSON2=Mahi
+DATE=$(date)
+
 
 echo "$PERSON1  hi how are you $PERSON2"
+echo "$PERSON2  WHAT ABOUT YOU $PERSON1"
+echo "$PERSON1 WHATS THE TIME NOW"
+echo "$PERSON2 THE TIME IS ${DATE}"
