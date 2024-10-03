b68f0cbf4859e7347c26d7629009feb721ee9585
Jagadish2536 <Jagadishvarma99@gmail.com>
2024-10-03 18:40:38 +0530

    script

--- Script.sh
+++ Script.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "jagadish"
+PERSON1=Mahi
+PERSON2=Jagadish
+DATE=$(pwd)
+
+echo "$PERSON1 loves $PERSON2" 
+echo "$DATE"
+
+
--- variables.sh
+++ variables.sh
@@ -9,3 +9,4 @@ echo "$PERSON1  hi how are you $PERSON2"
 echo "$PERSON2  WHAT ABOUT YOU $PERSON1"
 echo "$PERSON1 WHATS THE TIME NOW"
 echo "$DATE"
+
