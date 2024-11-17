0259c21e654f1ca486614c0ac35fdc5c3aec36e2
Jagadish2536 <Jagadishvarma99@gmail.com>
2024-11-17 10:02:58 +0530

    auto script

--- Script.sh
+++ Script.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
-PERSON1=Mahi
-PERSON2=Jagadish
-DATE=$(pwd)
+AMI=
+SG_ID=
+INSTANCES=("Mongodb" "Shipping" "MySQL" "Redis" "Catalogue" "User" "cart" "Web" "Rabbitmq" "Payment" "Dispatch")
+ZONE_ID=
+DOMAIN_NAME=
 
-echo "$PERSON1 loves $PERSON2" 
-echo "$DATE"
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "Mongodb" ] || [ $i == "Shipping" ] || [ $i == "MySQL" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i : $IP_ADDRESS"
 
-
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'" $i "'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'" $IP_ADDRESS "'"
+        }]
+      }
+    }]
+  }
+  '
+done
---
+++ cal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "addtion press 1"
+echo "sub press 2"
+echo "mul press 3"
+echo "div press 4"
+read choice
+
+case $choice in
+1) 
+  echo "please enter first value"
+  read a
+  echo $a
+  ;;
+esac
+
